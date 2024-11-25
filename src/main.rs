7e2170bf2c044f35fbbcb49f645b7c8957b9f439
Arrayfish <uekusa_hideaki@yahoo.co.jp>
2024-11-25 18:56:20 +0900

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+use axum::{
+    routing::get, 
+    Router, 
+    response::Json
+};
+use std::net::SocketAddr;
+use tokio::net::TcpListener;
+use serde_json::{Value, json};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    // pass incoming GET requests on "/hello-world" to "hello_world" handler.
+    let app = Router::new()
+        .route("/hello-world", get(hello_world))
+        .route("/todo", get(todo));
+
+    // write address like this to not make typos
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let listener = TcpListener::bind(addr).await?;
+
+    axum::serve(listener, app.into_make_service()).await?;
+
+    Ok(())
+}
+
+async fn hello_world() -> &'static str {
+    "Hello, world!"
+}
+
+async fn todo() -> Json<Value> {
+    Json(json!([
+        {
+            "content": "clean my room",
+            "done": false
+        },
+        {
+            "content": "Bye the apple",
+            "done": false
+        },] 
+    ))
+}
